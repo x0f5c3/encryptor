b80b58e35a67dde7ce5aa774201912874efad947
x0f5c3 <x0f5c3@tutanota.com>
2022-02-17 12:47:35 +0100

    Second

--- src/argon.rs
+++ src/argon.rs
@@ -1,14 +1,13 @@
 use anyhow::Result;
 use argon2::{password_hash::{
 	rand_core::OsRng,
-	PasswordHasher, PasswordVerifier, SaltString
-}, Argon2, Algorithm, Params};
-use argon2::Version::V0x13;
+	PasswordHasher, SaltString
+}, Argon2};
 
 
 pub fn create_password(password: &str) -> Result<String> {
 	let salt = SaltString::generate(&mut OsRng);
-	let argon = Argon2::new(Algorithm::Argon2id, V0x13, Params::default());
+	let argon = Argon2::default();
 	argon.hash_password(password.as_bytes(), &salt)
-		.map(|x| x.to_string()).into()
+		.map(|x| x.to_string()).map_err(|x| x.into())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+#![allow(dead_code)]
 mod argon;
 mod chacha;
 mod rsa;
+mod totp;
 
 fn main() {
     println!("Hello, world!");
